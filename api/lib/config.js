924022e467c80ba02dc38aeef6eb46085e60e58d
Jesus Dominguez <jesus.dominguez@kcl.ac.uk>
2022-10-21 10:53:52 +0100

    added default env args

--- api/lib/config.js
+++ api/lib/config.js
@@ -1,9 +1,10 @@
 module.exports = {
-	JENA_HOST: process.env.JENA_HOST,
-	JENA_PORT: process.env.JENA_PORT,
-	PROLOG_HOST: process.env.PROLOG_HOST,
-	PROLOG_PORT: process.env.PROLOG_PORT,
+	JENA_HOST: (process.env.JENA_HOST || "localhost"),
+	JENA_PORT: (process.env.JENA_PORT || "3030"),
+	PROLOG_HOST: (process.env.PROLOG_HOST || "localhost"),
+	PROLOG_PORT: (process.env.PROLOG_PORT || "1234"),
 	INSERT: "INSERT",
 	DELETE: "DELETE",
-	FUSEKI_PASSWORD: process.env.FUSEKI_PASSWORD
+	FUSEKI_PASSWORD: (process.env.FUSEKI_PASSWORD || "road2h"),
+	PORT: (process.env.PORT || "8888")
 };
